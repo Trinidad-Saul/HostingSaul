4cfde8255f99cc9cc9fb729faee986a6a4130112
Saul <saulatylano125mail.com>
2021-01-14 12:10:09 -0600

    Agregar archivos de proyecto.

---
+++ UTTT.Ejemplo.Persona.Control/Ctrl/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UTTT.Ejemplo.Persona.Control.Ctrl
+{
+    public class Conexion
+    {
+        #region Variables
+     
+        #endregion
+
+        #region Constructores
+        public Conexion()
+        {
+        }       
+        #endregion
+
+
+        public SqlConnection sqlConnection()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection("Data Source=SQL5101.site4now.net;Initial Catalog=DB_A6DB8C_Manual;User Id=DB_A6DB8C_Manual_admin;Password=Manual1234");
+                return conexion;
+            }
+            catch (Exception _e)
+            { 
+            
+            }
+            return null;
+        }
+    }
+}
